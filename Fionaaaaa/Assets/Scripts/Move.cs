1960187473f72e683291883452c6fa5b61716d48
Unknown <razeltif@gmail.com>
2019-10-30 21:54:02 +0100

    [ADD] pathfinding on click

---
+++ Fionaaaaa/Assets/Scripts/Move.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Move : MonoBehaviour
+{
+
+    private NavMeshAgent navMesh;
+
+    public float speed = 0.5f;
+    public float minimumDistanceToValidateDestination = 2;
+
+    private void Awake()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMesh.speed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            navMesh.destination  = getMouseClickPosition();
+
+        }
+        
+    }
+
+
+    private Vector3 getMouseClickPosition()
+    {
+        Vector3 pos = new Vector3();
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit, 500))
+        {
+            pos = hit.point;
+        }
+
+        return pos;
+    }
+
+
+
+    private void OnValidate()
+    {
+        if(navMesh != null)
+            navMesh.speed = speed;
+    }
+}
