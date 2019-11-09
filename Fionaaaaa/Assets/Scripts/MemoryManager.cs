0dfe3ac04f309be7aeec52787c4819598b773e67
Unknown <razeltif@gmail.com>
2019-11-09 18:39:48 +0100

    [ADD] les memoires sont impactées par l'humeur

--- Fionaaaaa/Assets/Scripts/MemoryManager.cs
+++ Fionaaaaa/Assets/Scripts/MemoryManager.cs
@@ -26,9 +26,9 @@ public class MemoryManager : MonoBehaviour
 
     public void OnClickMemory()
     {
-        if(state < settings.memoireCalme.memories.Length)
+        if(state < getMemoriesEnergies().memories.Length)
         {
-            DialogueVisual.instance.startNewSentence(true, settings.memoireCalme.memories[state]);
+            DialogueVisual.instance.startNewSentence(true, getMemoriesEnergies().memories[state]);
             state++;
         }
         else
@@ -44,7 +44,7 @@ public class MemoryManager : MonoBehaviour
         this.settings = settings;
         DialogueVisual.instance.gameObject.SetActive(true);
 
-        DialogueVisual.instance.startNewSentence(true, settings.memoireCalme.memories[0]);
+        DialogueVisual.instance.startNewSentence(true, getMemoriesEnergies().memories[0]);
         state++;
     }
 
@@ -54,5 +54,27 @@ public class MemoryManager : MonoBehaviour
         DialogueVisual.instance.gameObject.SetActive(false);
         state = 0;
         GameManager.instance.gameState = GameState.free;
+    }
+
+    memoriesEnergies getMemoriesEnergies()
+    {
+
+        switch (HumorManager.instance.actualHumor)
+        {
+            case HumorState.anxieuse:
+                return settings.memoireAnxieuse;
+
+            case HumorState.calme:
+                return settings.memoireCalme;
+
+            case HumorState.colerique:
+                return settings.memoireColerique;
+
+            default:
+                return settings.memoireCalme;
+
+        }
+
+
     }
 }
